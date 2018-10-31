48f44e0b02b75bd1b21c0787732faea79809dcd2
e2dubba <sgoodwin@atla.com>
2018-10-31 08:34:57 -0500

    additions to syllables to help normalize vowels

--- scripts/AkkWord.py
+++ scripts/AkkWord.py
@@ -7,11 +7,33 @@ CONSENANTS = {'ʾ', 'b',
         'q', 'r', 's', 
         'ṣ', 'š', 't', 
         'ṭ', 'w', 'z'}
+
+MORPHEMIC_CONS = {'t', 'ṭ', 'd', 
+        'š', 'n'}
+
 VOWELS = {'a', 'ā', 'á', 'à', 'â',
         'e', 'ē', 'é', 'è', 'â',
         'i', 'ī', 'í', 'ì', 'î',
         'o', 'ō', 'ô', 
         'u', 'ū', 'ú', 'ù', 'û'}
+
+VOWELDICT = {'a': ['ā', 'á', 'à', 'â'],
+        'e': ['ē', 'é', 'è', 'â'],
+        'i': ['ī', 'í', 'ì', 'î'],
+        'o': ['ō', 'ô'],
+        'u': ['ū', 'ú', 'ù', 'û']}
+
+VOWELTRANS = vowel_translator()
+VT = str.maketrans(VOWELTRANS)
+
+
+def vowel_translator():
+    voweltrans = {}
+    for key in VOWELDICT.keys():
+        voweltrans[key] = key
+        for vowel in VOWELDICT[key]:
+            voweltrans[vowel] = key
+    return voweltrans
 
 
 class AkkWord:
@@ -26,29 +48,21 @@ class AkkWord:
     
 
     def __syllabifier(self, signs):
-        sign_dict = {k:v for k, v in enumerate(signs)}
-        syllable_list = [[signs[0]]]
-        if signs[1][0] in VOWELS:
-            #and if signs[0][1] == previous vowel
-                syllable_list[0].append(signs[1])
+        sign_dict = {k:v.translate(VT) for k, v in enumerate(signs)}
+        syllable_list = [[sign_dict[0]]]
         for i in range(1,len(signs)):
             if sign_dict[i][0] in CONSENANTS:
                 syllable_list.append([sign_dict[i]])
             elif sign_dict[i][0] == sign_dict[i-1][-1]:
                 syllable_list[-1].append(sign_dict[i])
             else:
-                syllable_list.append([sign_dict[i]])
+                syllable_list.append(['ʾ'+sign_dict[i]])
 
         return syllable_list
 
 
-
-
-            
-        
-
-
-
+    def extract_root(self, syllables):
+        for 
 
 
 
